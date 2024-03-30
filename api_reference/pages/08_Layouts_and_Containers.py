59969a04900c25169e38bdf6cfd5aae236fcc4b2
harrychangjr <harrychang@u.nus.edu>
2024-03-30 14:45:40 +0800

    till 09

--- api_reference/pages/08_Layouts_and_Containers.py
+++ api_reference/pages/08_Layouts_and_Containers.py
@@ -1,5 +1,203 @@
 import streamlit as st
+import numpy as np
+import time
 
 st.header("Layouts and Containers")
 
 st.sidebar.markdown("Layouts and Containers")
+
+st.subheader("st.columns")
+
+st.write("""
+
+Insert containers laid out as side-by-side columns.
+
+Inserts a number of multi-element containers laid out side-by-side and returns a list of container objects.
+
+To add elements to the returned containers, you can use the "with" notation (preferred) or just call methods directly on the returned object. See examples below.
+
+Columns can only be placed inside other columns up to one level of nesting.
+
+
+""")
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+   st.header("A cat")
+   st.image("https://static.streamlit.io/examples/cat.jpg")
+
+with col2:
+   st.header("A dog")
+   st.image("https://static.streamlit.io/examples/dog.jpg")
+
+with col3:
+   st.header("An owl")
+   st.image("https://static.streamlit.io/examples/owl.jpg")
+
+col1, col2 = st.columns([3, 1])
+data = np.random.randn(10, 1)
+
+col1.subheader("A wide column with a chart")
+col1.line_chart(data)
+
+col2.subheader("A narrow column with the data")
+col2.write(data)
+
+st.subheader("st.container")
+
+st.write("""
+
+Insert a multi-element container.
+
+Inserts an invisible container into your app that can be used to hold multiple elements. This allows you to, for example, insert multiple elements into your app out of order.
+
+To add elements to the returned container, you can use the "with" notation (preferred) or just call methods directly on the returned object. See examples below.
+
+
+""")
+
+with st.container():
+   st.write("This is inside the container")
+
+   # You can call any Streamlit command, including custom components:
+   st.bar_chart(np.random.randn(50, 3))
+
+st.write("This is outside the container")
+
+container = st.container(border=True)
+container.write("This is inside the container")
+st.write("This is outside the container")
+
+# Now insert some more in the container
+container.write("This is inside too")
+
+row1 = st.columns(3)
+row2 = st.columns(3)
+
+for col in row1 + row2:
+    tile = col.container(height=120)
+    tile.title(":balloon:")
+
+long_text = "Lorem ipsum. " * 1000
+
+with st.container(height=300):
+    st.markdown(long_text)
+
+st.subheader("st.empty")
+
+st.write("""
+
+Insert a single-element container.
+
+Inserts a container into your app that can be used to hold a single element. This allows you to, for example, remove elements at any point, or replace several elements at once (using a child multi-element container).
+
+To insert/replace/clear an element on the returned container, you can use "with" notation or just call methods directly on the returned object. See examples below.
+
+
+""")
+
+st.code('''
+with st.empty():
+    for seconds in range(60):
+        st.write(f"⏳ {seconds} seconds have passed")
+        time.sleep(1)
+    st.write("✔️ 1 minute over!")
+''', language='python')
+
+st.subheader("st.expander")
+
+st.write("""
+
+Insert a multi-element container that can be expanded/collapsed.
+
+Inserts a container into your app that can be used to hold multiple elements and can be expanded or collapsed by the user. When collapsed, all that is visible is the provided label.
+
+To add elements to the returned container, you can use the "with" notation (preferred) or just call methods directly on the returned object. See examples below.
+
+
+""")
+
+st.bar_chart({"data": [1, 5, 2, 6, 2, 1]})
+
+with st.expander("See explanation"):
+    st.write("""
+        The chart above shows some numbers I picked for you.
+        I rolled actual dice for these, so they're *guaranteed* to
+        be random.
+    """)
+    st.image("https://static.streamlit.io/examples/dice.jpg")
+
+st.subheader("st.popover")
+
+st.write("""
+
+Insert a popover container.
+
+Inserts a multi-element container as a popover. It consists of a button-like element and a container that opens when the button is clicked.
+
+Opening and closing the popover will not trigger a rerun. Interacting with widgets inside of an open popover will rerun the app while keeping the popover open. Clicking outside of the popover will close it.
+
+To add elements to the returned container, you can use the "with" notation (preferred) or just call methods directly on the returned object. See examples below.
+
+""")
+
+with st.popover("Open popover"):
+    st.markdown("Hello World 👋")
+    name = st.text_input("What's your name?")
+
+st.write("Your name:", name)
+
+st.subheader("st.sidebar")
+
+st.write("""
+
+Not only can you add interactivity to your app with widgets, you can organize them into a sidebar. Elements can be passed to `st.sidebar` using object notation and `with` notation.
+
+""")
+
+# Using object notation
+add_selectbox = st.sidebar.selectbox(
+    "How would you like to be contacted?",
+    ("Email", "Home phone", "Mobile phone")
+)
+
+# Using "with" notation
+with st.sidebar:
+    add_radio = st.radio(
+        "Choose a shipping method",
+        ("Standard (5-15 days)", "Express (2-5 days)")
+    )
+
+with st.sidebar:
+    with st.echo():
+        st.write("This code will be printed to the sidebar.")
+
+    with st.spinner("Loading..."):
+        time.sleep(5)
+    st.success("Done!")
+
+st.subheader("st.tabs")
+
+st.write("""
+
+Insert containers separated into tabs.
+
+Inserts a number of multi-element containers as tabs. Tabs are a navigational element that allows users to easily move between groups of related content.
+
+To add elements to the returned containers, you can use the "with" notation (preferred) or just call methods directly on the returned object. See examples below.
+""")
+
+tab1, tab2, tab3 = st.tabs(["Cat", "Dog", "Owl"])
+
+with tab1:
+   st.header("A cat")
+   st.image("https://static.streamlit.io/examples/cat.jpg", width=200)
+
+with tab2:
+   st.header("A dog")
+   st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
+
+with tab3:
+   st.header("An owl")
+   st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
--- api_reference/pages/09_Chat_Elements.py
+++ api_reference/pages/09_Chat_Elements.py
@@ -1,5 +1,102 @@
 import streamlit as st
+import numpy as np
+import pandas as pd
+import time
 
 st.header("Chat Elements")
 
 st.sidebar.markdown("Chat Elements")
+
+st.subheader("st.chat_input")
+
+st.write("Display a chat input widget.")
+
+prompt = st.chat_input("Say something")
+if prompt:
+    st.write(f"User has sent the following prompt: {prompt}")
+
+st.code("""
+#Alternative
+with st.sidebar:
+    messages = st.container(height=300)
+    if prompt := st.chat_input("Say something"):
+        messages.chat_message("user").write(prompt)
+        messages.chat_message("assistant").write(f"Echo: {prompt}")
+"""
+)
+
+st.subheader("st.chat_message")
+
+st.write("""
+
+Insert a chat message container.
+
+To add elements to the returned container, you can use `with` notation (preferred) or just call methods directly on the returned object. See the examples below.
+
+""")
+
+with st.chat_message("user"):
+    st.write("Hello 👋")
+    st.line_chart(np.random.randn(30, 3))
+
+message = st.chat_message("assistant")
+message.write("Hello human")
+message.bar_chart(np.random.randn(30, 3))
+
+st.subheader("st.status")
+
+st.write("""
+
+Insert a status container to display output from long-running tasks.
+
+Inserts a container into your app that is typically used to show the status and details of a process or task. The container can hold multiple elements and can be expanded or collapsed by the user similar to st.expander. When collapsed, all that is visible is the status icon and label.
+
+The label, state, and expanded state can all be updated by calling .update() on the returned object. To add elements to the returned container, you can use "with" notation (preferred) or just call methods directly on the returned object.
+
+By default, st.status() initializes in the "running" state. When called using "with" notation, it automatically updates to the "complete" state at the end of the "with" block. See examples below for more details.
+
+""")
+
+with st.status("Downloading data..."):
+    st.write("Searching for data...")
+    time.sleep(2)
+    st.write("Found URL.")
+    time.sleep(1)
+    st.write("Downloading data...")
+    time.sleep(1)
+
+st.button('Rerun')
+
+st.subheader("st.write_stream")
+
+st.write("""
+
+Stream a generator, iterable, or stream-like sequence to the app.
+
+`st.write_stream` iterates through the given sequences and writes all chunks to the app. String chunks will be written using a typewriter effect. Other data types will be written using `st.write`.
+""")
+
+_LOREM_IPSUM = """
+Lorem ipsum dolor sit amet, **consectetur adipiscing** elit, sed do eiusmod tempor
+incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+"""
+
+
+def stream_data():
+    for word in _LOREM_IPSUM.split(" "):
+        yield word + " "
+        time.sleep(0.02)
+
+    yield pd.DataFrame(
+        np.random.randn(5, 10),
+        columns=["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"],
+    )
+
+    for word in _LOREM_IPSUM.split(" "):
+        yield word + " "
+        time.sleep(0.02)
+
+
+if st.button("Stream data"):
+    st.write_stream(stream_data)
