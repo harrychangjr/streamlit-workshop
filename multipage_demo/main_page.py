287d68238ed335fba1aa0f30b7437d9e466e09e3
Gordon Tham Qi Hao <92292546+gorttham@users.noreply.github.com>
2024-05-27 13:48:22 +0800

    Add explainations in main_page

--- multipage_demo/main_page.py
+++ multipage_demo/main_page.py
@@ -14,10 +14,11 @@ Multipage Streamlit apps are useful when we want to organise our data and functi
 To get started, you just need to have the main_page and another folder containing the scripts for the other pages
 
 This section showcases:
-    1. Using multiple pages
-    2. Streamlit Components
+    1. Using multiple pages (file structure and persisting variables)
+    2. Waiting for excecution
     3. Sample Data Uses
 
+              main_page            page_2             page_3
 Based on: [Data Collection] -> [Data Analysis] -> [Data Modelling]
 
 Let's Begin!
@@ -27,19 +28,30 @@ with st.sidebar:
     st.title('Main page 🎈')
     st.caption("First Page")
 
-with st.form(key='confirm_upload'): # form identifier
+def confirm_upload():
+    st.write("Data Uploaded!")
+    
+# Form groups elements is built with a Submit button. 
+# Values will be sent as a batch when Submit button is pressed.
+# If Widgets are not in a form, Streamlit script will rerun everytime the value is changed (Useful for "pausing" excecution)
+with st.form(key='confirm_upload'):
     file = st.file_uploader("Upload file here:", accept_multiple_files = False) #literally uploads the bytes data
-    submit_button = st.form_submit_button(label='Submit')
-    
-if file:
-    st.session_state["uploaded_file"] = pd.read_csv(file)
-    
+    submit_button = st.form_submit_button(label='Submit', on_click = confirm_upload)
 
-# Process the uploaded files
-if file:
-    if file.type == "text/csv" or file.type == "text/xlsx":
-        file_details = {"FileName": file.name, "FileType": file.type, "FileSize": file.size}
-        st.write("You have uploaded: ", file_details)
-        
-    else:
-        st.write("Please upload valid file and check file type!")
+if submit_button: # check that button has been pressed
+    if file: # check data is available
+
+        # Process the uploaded files
+        if file.type == "text/csv" or file.type == "text/xlsx":
+            file_details = {"FileName": file.name, "FileType": file.type, "FileSize": file.size}
+            st.write("You have uploaded: ", file_details)
+
+            #persist data within the session
+            try:
+                st.session_state["uploaded_file"] = pd.read_csv(file)
+            except:
+                st.session_state["uploaded_file"] = pd.read_xlsx(file)
+            
+        else:
+            st.write("Please upload valid file and check file type!")
+    
